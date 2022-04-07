ab53e14660cf7274f5cccf4c8d4ea3b1557a274e
Olga Bielkina <belkinaolga88@gmail.com>
2022-04-07 20:29:29 +0200

    add comment input with validation

--- script.js
+++ script.js
@@ -30,4 +30,63 @@ colorElements.forEach((element, index) => {
             image.setAttribute('src', './assets/' + color + '_socks.jpeg')
         }
     })
-})
+});
+
+const MAX_LENGHT_ERROR = 'MAX_LENGHT_ERROR'
+const MIN_LENGHT_ERROR = 'MIN_LENGHT_ERROR'
+const REQUIRED_ERROR = 'REQUIRED_ERROR'
+
+const validateInput = (str='') => {
+    if(str.length === 0) {
+        return REQUIRED_ERROR
+    }
+
+    if(str.length < 5) {
+        return MIN_LENGHT_ERROR
+    }
+
+    if(str.length > 15) {
+        return MAX_LENGHT_ERROR
+    }
+}
+
+const commentElement = document.querySelector('.commentInput');
+
+if(commentElement) {
+    commentElement.addEventListener('change', (event) => {
+        const text = event.target.value;
+
+        const error = validateInput(text);
+        const errorElement = document.querySelector('.error');
+        if (error) {
+            let errorText = ''
+            switch(error) {
+                case REQUIRED_ERROR:
+                    errorText = 'comment is required';
+                    break;
+                case MIN_LENGHT_ERROR:
+                case MAX_LENGHT_ERROR:
+                    errorText = 'comment should be between 5 and 10 characters'
+                    break;
+                default:
+                    errorText = 'comment is invalid'
+            }
+            
+            errorElement.innerHTML = errorText
+            event.target.classList.add('inputError')
+        } else {
+            const commentsContainer = document.querySelector('.comments');
+
+            if(commentsContainer) {
+                const p = document.createElement('p');
+                p.classList.add('comment');
+                p.innerHTML = text;
+
+                commentsContainer.appendChild(p);
+                event.target.value = ''
+                errorElement.innerHTML = ''
+                event.target.classList.remove('inputError')
+            }
+        }
+    });
+}
